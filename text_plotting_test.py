0d3f667cb985951808dcc83c6a9dad452ca42b7a
Volodymyr (Vlad) Chapman <44582194+VolodymyrChapman@users.noreply.github.com>
2021-10-27 10:49:46 +0100

    Update text_plotting_test.py

--- text_plotting_test.py
+++ text_plotting_test.py
@@ -63,7 +63,7 @@ print(test_cipher)  #print cipher used - cipher starts at top of the radius 1 ci
                # angle clockwise around the circle (with respect to the origin (0,0) )
 
 
-# Input text
+# Input text - borrowed with respect and admiration from Mary Shelley's Frankenstein
 sample_text = """You will rejoice to hear that no disaster has accompanied the commencement of an enterprise which you have regarded with such evil forebodings. 
 I arrived here yesterday, and my first task is to assure my dear sister of my welfare and increasing confidence in the success of my undertaking.
 I am already far north of London, and as I walk in the streets of Petersburgh, I feel a cold northern breeze play upon my cheeks, 
